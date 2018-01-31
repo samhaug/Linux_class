bb75cee0c7468cf81a1dd0ef514a4818950bf351
Samuel Haugland <samhaug@umich.edu>
2018-01-31 10:26:56 -0500

    commit

--- interp_polyfit/make_plot.py
+++ interp_polyfit/make_plot.py
@@ -6,7 +6,7 @@
 File Name : make_plot.py
 Purpose : ---
 Creation Date : 30-01-2018
-Last Modified : Tue 30 Jan 2018 05:13:30 PM EST
+Last Modified : Wed 31 Jan 2018 10:26:06 AM EST
 Created By : Samuel M. Haugland
 
 ==============================================================================
@@ -22,6 +22,15 @@ from matplotlib.collections import PatchCollection
 def main():
     fig,ax = setup_figure()
     plt.figtext(0.05,0.9,'A',size=20)
+    ax.text(0.084092,1.413547,'Skarn',size=12)
+    ax.text(1.95006,0.43502,'Fe-Ti,V',size=12)
+    #rotated Skarn/Porphyry text
+    ax.text(0.72314,5.70849,'Skarn',size=12,rotation=65,
+            backgroundcolor='white')
+    ax.text(1.52314,5.70849,'Porphyry',size=12,rotation=65,
+            backgroundcolor='white')
+    ax.text(2.52314,4.50849,'(Nadoli et al.\nsubmitted)',size=8,rotation=65,
+            backgroundcolor='white')
 
     plot_red_triangles(ax,'red_triangle_coords')
     plot_green_triangles(ax,'green_triangle_coords')
@@ -32,9 +41,16 @@ def main():
 
     fit_skarn_porphyry(ax,'skarn_porphyry_curve')
 
+    horiz = np.genfromtxt('horizontal_line',delimiter=',')
+    ax.plot(horiz[:,0],horiz[:,1],color='k',lw=2)
+
+    slant = np.genfromtxt('slant_line',delimiter=',')
+    ax.plot(slant[:,0],slant[:,1],color='k',lw=2)
+
     plot_porphyry_poly(ax,'porphyry_coords')
     plot_IOGC_poly(ax,'IOGC_coords')
     plot_BIF_poly(ax,'BIF_coords')
+    plot_kiruna_poly(ax,'kiruna_coords')
     plt.savefig('al_mn_plot.pdf')
 
     plt.show()
@@ -44,6 +60,19 @@ def plot_porphyry_poly(ax,fname):
     polygon = Polygon(coords,True)
     p = PatchCollection([polygon],lw=2,facecolor='none',
                          edgecolor='k')
+    ax.annotate('Porphyry',xy=(0.67334,0.24271),xytext=(1.60416,0.17219),size=12,
+            arrowprops=dict(facecolor='black',width=0.5,headwidth=3,
+                            headlength=5))
+    ax.add_collection(p)
+
+def plot_kiruna_poly(ax,fname):
+    coords = np.genfromtxt(fname,delimiter=',')
+    polygon = Polygon(coords,True)
+    p = PatchCollection([polygon],lw=2,facecolor='none',
+                         edgecolor='k',linestyle='-')
+    ax.annotate('Kiruna',xy=(0.34484,0.08374),xytext=(0.149122,0.03315),size=12,
+            arrowprops=dict(facecolor='black',width=0.5,headwidth=3,
+                            headlength=5))
     ax.add_collection(p)
 
 def plot_BIF_poly(ax,fname):
@@ -52,15 +81,16 @@ def plot_BIF_poly(ax,fname):
     p = PatchCollection([polygon],lw=2,facecolor='none',
                          edgecolor='k',linestyle='--')
     ax.add_collection(p)
-    ax.text(0.02,0.05,'BIF',size=14)
+    ax.text(0.02261,0.054844,'BIF',size=12)
 
 def plot_IOGC_poly(ax,fname):
     coords = np.genfromtxt(fname,delimiter=',')
     polygon = Polygon(coords,True)
     p = PatchCollection([polygon],lw=2,facecolor='none',edgecolor='k')
     ax.add_collection(p)
-    ax.annotate('IOGC',xy=(0.1,0.1),xytext=(0.01,0.14),size=14,
-            arrowprops=dict(facecolor='black',width=1,headwidth=5))
+    ax.annotate('IOGC',xy=(0.067246,0.155347),xytext=(0.011401,0.170237),
+                size=12,arrowprops=dict(facecolor='black',width=0.5,
+                                        headwidth=3,headlength=5))
 
 def fit_skarn_porphyry(ax,fname):
     coords = np.genfromtxt(fname,delimiter=',')
@@ -83,22 +113,30 @@ def fit_skarn_porphyry(ax,fname):
 def plot_red_triangles(ax,fname):
     coords = np.genfromtxt(fname,delimiter=',')
     ax.scatter(coords[:,0],coords[:,1],marker='v',color='r',facecolors='none',
-              edgecolors='r',s=60,alpha=0.6)
+              edgecolors='firebrick',s=60,alpha=0.6)
+    ax.scatter(np.mean(coords[:,0]),np.mean(coords[:,1]),marker='+',s=500,
+               color='firebrick')
 
 def plot_green_triangles(ax,fname):
     coords = np.genfromtxt(fname,delimiter=',')
     ax.scatter(coords[:,0],coords[:,1],marker='^',facecolors='none',
               edgecolors='olive',s=60,alpha=0.6)
+    ax.scatter(np.mean(coords[:,0]),np.mean(coords[:,1]),marker='+',s=500,
+               color='olive')
 
 def plot_yellow_squares(ax,fname):
     coords = np.genfromtxt(fname,delimiter=',')
     ax.scatter(coords[:,0],coords[:,1],marker='s',facecolors='none',
               edgecolors='goldenrod',s=60,alpha=0.6)
+    ax.scatter(np.mean(coords[:,0]),np.mean(coords[:,1]),marker='+',s=500,
+               color='goldenrod')
 
 def plot_blue_circles(ax,fname,alpha):
     coords = np.genfromtxt(fname,delimiter=',')
     ax.scatter(coords[:,0],coords[:,1],marker='o',facecolors='none',
               edgecolors='steelblue',s=60,alpha=alpha)
+    ax.scatter(np.mean(coords[:,0]),np.mean(coords[:,1]),marker='+',s=500,
+               color='steelblue')
 
 def plot_purple_diamonds(ax,fname):
     coords = np.genfromtxt(fname,delimiter=',')
@@ -106,7 +144,7 @@ def plot_purple_diamonds(ax,fname):
               edgecolors='purple',s=60,alpha=1.0)
 
 def setup_figure():
-    fig,ax = plt.subplots(figsize=(6,6))
+    fig,ax = plt.subplots(figsize=(7,7))
     ax.set_xscale("log", nonposx='clip')
     ax.set_yscale("log", nonposy='clip')
     ax.set_xlim(0.0001,10)
